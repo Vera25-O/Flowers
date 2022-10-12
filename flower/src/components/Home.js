1011692021658d62c6c302c4c8d58cb2b6042cf7
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-10-12 19:38:15 +0300

    modified Home.js

--- flower/src/components/Home.js
+++ flower/src/components/Home.js
@@ -3,10 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <div className='home'>
-      <h1>ABOUT</h1>
+      
       <h1>FLOWER FOR LIFE</h1>
     
-    <img src="" alt="flower" /><br/>
+    <img src="https://image.shutterstock.com/shutterstock/photos/2135017821/display_1500/stock-photo-abstract-contemporary-art-wide-panoramic-collage-portrait-of-young-woman-with-flowers-2135017821.jpg" alt="flower" /><br/>
     
       </div>
   )
