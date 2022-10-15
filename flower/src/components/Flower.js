57bca16c3cb5a4ced5f8a07559439c132ea38f65
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-10-15 10:51:55 +0300

    modified Flower.js

--- flower/src/components/Flower.js
+++ flower/src/components/Flower.js
@@ -2,27 +2,10 @@ import React from "react";
 import { useState } from "react";
 
 function Flower({ id, name, description, image, favorite, updateFlowers, handleDelete }) {
-  const [likes, setLikes] = useState(0);
+  
+  
 
-  function coutLikes() {
-    setLikes(likes + 1);
-  }
-
-  function addToFavourite() {
-    fetch(`/flowers/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "Application/json",
-        Accept: "Application/json",
-      },
-      body: JSON.stringify({ favourite: !favorite }),
-    })
-      .then((response) => response.json())
-      .then((data) => updateFlowers(data))
-
-      .catch((error) => console.log(error));
-  }
-
+  
   function addToDelete() {
     fetch(`/flowers/${id}`, {
       method: "DELETE"
@@ -40,13 +23,9 @@ function Flower({ id, name, description, image, favorite, updateFlowers, handleD
       <p>
         <strong>{description}</strong>
       </p>
-      <button onClick={coutLikes} className="like-button">
-        Likes {likes} ♥
-      </button>
+      
 
-      <button onClick={addToFavourite}>
-        {favorite ? "Remove Favourite" : "Add Favourite"}
-      </button>
+      
 
       <button onClick={addToDelete}>
         Delete
